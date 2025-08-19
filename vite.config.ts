6536fef983a3e6b93cbd07493e9aa319d818bade
hyojunxx <tayambunso27@gmail.com>
2025-08-20 07:42:54 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/diogenes-tayam-portfolio/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
