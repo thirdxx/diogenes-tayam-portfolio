53d8cf3a286f4a3b4073629563f6ec17556d30f2
Diogenes Tayam <125981474+thirdxx@users.noreply.github.com>
2025-08-20 09:07:07 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
